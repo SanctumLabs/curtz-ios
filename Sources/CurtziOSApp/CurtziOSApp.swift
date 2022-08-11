67aa0025f691a885894423961ae1f2f1d923769e
Dev Agani <dev.things.ke@gmail.com>
2022-08-11 17:08:04 +0200

    add curtz-ios app files and folder structure

---
+++ Sources/CurtziOSApp/CurtziOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurtziOSApp.swift
+//  CurtziOS
+//
+//  Created by George Nyakundi on 11/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CurtziOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
