26291364e08f1eac75d8640adf8a79e5901ba52e
Dev Agani <dev.things.ke@gmail.com>
2022-08-29 23:21:26 +0200

    Use a factory method to create the SUT

--- Tests/Curtz/URLSessionHTTPClientTests.swift
+++ Tests/Curtz/URLSessionHTTPClientTests.swift
@@ -48,7 +48,7 @@ class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
         
-        let sut = URLSessionHTTPClient()
+        let sut = makeSUT()
         sut.perform(request: urlRequest) { _ in }
         wait(for: [exp], timeout: 1.0)
        
@@ -60,7 +60,7 @@ class URLSessionHTTPClientTests: XCTestCase {
         let error = NSError(domain: "any error", code: 1)
         
         URLProtocolStub.stub(data: nil, response: nil, error: error)
-        let sut = URLSessionHTTPClient()
+        let sut = makeSUT()
         
         let exp = expectation(description: "wait for completion")
         sut.perform(request: urlRequest) { result in
@@ -76,6 +76,10 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     // MARK: - Helpers
+    
+    private func makeSUT() -> URLSessionHTTPClient {
+        return URLSessionHTTPClient()
+    }
     
     private class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
