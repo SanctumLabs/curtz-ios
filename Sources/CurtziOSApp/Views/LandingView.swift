da774df01b697c3ca31f9d01b8d5969b779e2452
Dev Agani <dev.things.ke@gmail.com>
2023-03-09 15:02:22 +0100

    Update project generation to also include a launch screen

---
+++ Sources/CurtziOSApp/Views/LandingView.swift
@@ -0,0 +1,20 @@
+//
+//  LandingView.swift
+//  CurtzAllTests
+//
+//  Created by George Nyakundi on 03/02/2023.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        Text(verbatim: "Someon")
+    }
+}
+
+struct LandingView_Preview: PreviewProvider {
+    static var previews: some View {
+        LandingView()
+    }
+}
