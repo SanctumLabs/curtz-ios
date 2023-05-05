2b94c5ad748e76a15c9a8ec2d8b1ec98ef34a57a
Dev Agani <dev.things.ke@gmail.com>
2023-05-05 13:32:19 +0200

    Rethinking `Store` implementation using a `StoreManager` to manage the store which remains to be an internal detail

--- Tests/Curtz/Token/CurtzTokenStoreUnitTests.swift
+++ Tests/Curtz/Token/CurtzTokenStoreUnitTests.swift
@@ -19,9 +19,61 @@ enum StoreError: Error {
 }
 
 protocol Store {
-    func retrieve()
+    func add()
+    func search()
+    func update()
+    func delete()
 }
 
-final class CurtzStore: Store {
-    func retrieve() {}
+/// <#Description#>
+protocol StoreManager {
+    
+}
+
+final class CurtzStoreManager: StoreManager {
+    
+    private let store: Store
+    
+    init(with store: Store){
+        self.store = store
+    }
+}
+
+final class CurtzStoreManagerUnitTests: XCTestCase {
+    
+    func test_init_doesNOT_performAnyAction() {
+        let (_,store) = makeSUT()
+        
+        XCTAssertEqual(store.messages, [])
+    }
+    
+    // MARK: - Private
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: StoreManager, store: MockStore) {
+        let store = MockStore()
+        let sut = CurtzStoreManager(with: store)
+        
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+    
+    private final class MockStore: Store {
+        
+        private(set) var messages: [StoreAction] = []
+        
+        func add() {}
+        
+        func search() {}
+        
+        func update() {}
+        
+        func delete() {}
+    }
+}
+
+enum StoreAction {
+    case add
+    case search
+    case update
+    case delete
 }
