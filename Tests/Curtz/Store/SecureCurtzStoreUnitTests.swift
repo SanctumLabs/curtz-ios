1dd607dcbccf331b72472278f4242480b641d513
Dev Agani <dev.things.ke@gmail.com>
2023-05-16 21:51:16 +0200

    Test drive `KeyChainStore`

---
+++ Tests/Curtz/Store/SecureCurtzStoreUnitTests.swift
@@ -0,0 +1,220 @@
+//
+//  SecureCurtzStoreUnitTests.swift
+//  CurtzUnitTests
+//
+//  Created by George Nyakundi on 12/05/2023.
+//
+
+import XCTest
+import Security
+import Curtz
+
+final class SecureCurtzStoreUnitTests: XCTestCase {
+    
+    func test_search_shouldReturnANotFoundErrorForANonExistentKey() {
+        let sut = makeSUT()
+        let key = "a-weird-key"
+        
+        sut.search(forKey: key) { result in
+            switch result {
+            case let .failure(error):
+                XCTAssertEqual(error, .notFound)
+            default:
+                XCTFail("Expected a failure but received something else")
+            }
+        }
+    }
+    
+    func test_add_shouldPersistInTheStore() {
+        let sut = makeSUT()
+        let valueToSave = "my-very-strong-password"
+        let key = "my-special-key"
+        
+        let expectation = expectation(description: "wait for save")
+        
+        sut.add(valueToSave, key: key) { _ in}
+        sut.search(forKey: key) { result in
+            switch result {
+            case let .success(valueRetrieved):
+                XCTAssertEqual(valueRetrieved, valueToSave)
+            case let .failure(error):
+                XCTFail("\(error)")
+            }
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 0.3)
+    }
+    
+    func test_update_shouldPersistNewValueIntheStore() {
+//        let sut = makeSUT()
+//        let initialValueToSave = "initial-value-to-save"
+//        let valueToUpdateTo = "heres-the-new value"
+//
+//        let key = "my-awesome-key"
+//
+//        let expectation = expectation(description: "wait for search")
+//
+//        // Add
+//        sut.add(initialValueToSave, key: key) { _ in }
+//        // Update
+//        sut.update(valueToUpdateTo, forKey: key) { _ in }
+//        // Fetch
+//        sut.search(forKey: key) { result in
+//            switch result {
+//            case let .success(retrievedValue):
+//                XCTAssertNotEqual(retrievedValue, initialValueToSave)
+//                XCTAssertEqual(retrievedValue, valueToUpdateTo)
+//            default:
+//                XCTFail("Update should be successful")
+//            }
+//            expectation.fulfill()
+//        }
+//
+//        wait(for: [expectation], timeout: 0.2)
+    }
+    
+    func test_delete_shouldDeleteARecordFromTheStore() {
+        let sut = makeSUT()
+        let value = "hatathasdfdafad"
+        let key = "a value with a short lifetime"
+        
+        let firstExpectation = XCTestExpectation(description: "wait for search")
+        let secondExpectation = XCTestExpectation(description: "wait for delete")
+        
+        sut.add(value, key: key) { _ in }
+        sut.search(forKey: key) { result in
+            switch result  {
+            case let .success(retrievedValue):
+                XCTAssertEqual(retrievedValue, value)
+            default:
+                XCTFail("Value should have been saved")
+            }
+            firstExpectation.fulfill()
+        }
+        sut.deleteValue(key: key) { _ in }
+        sut.search(forKey: key) { result in
+            switch result {
+            case let .failure(error):
+                XCTAssertEqual(error, .notFound)
+            default:
+                XCTFail("Value should have been deleted")
+            }
+            secondExpectation.fulfill()
+        }
+        
+        wait(for: [firstExpectation, secondExpectation], timeout: 0.2)
+    }
+    
+    
+    // MARK: - Helpers
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> KeyChainStore {
+        let sut = KeyChainStore()
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+    
+}
+
+
+// IMPLEMENT KeyChainStore
+
+final class KeyChainStore: Store {
+    
+    init() { /* no code required */}
+    
+    private let service = "com.sanctumlabs.curtz"
+    
+    func add(_ val: String, key: String, completion: @escaping (AddResult) -> Void) {
+        let query: [String: Any] = [
+            kSecAttrAccessible as String : kSecAttrAccessibleWhenUnlocked,
+            securityClass: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: key,
+            kSecValueData as String: val
+        ]
+        // Delete
+        SecItemDelete(query as CFDictionary)
+        
+        // then save
+        let status = SecItemAdd(query as CFDictionary, nil)
+        
+        switch status {
+        case errSecSuccess:
+            completion(.success(()))
+        default:
+            completion(.failure(.failedToSave))
+        }
+    }
+    
+    func search(forKey key: String, completion: @escaping (SearchResult) -> Void) {
+        let query: [String: Any] = [
+            securityClass: kSecClassGenericPassword,
+            kSecAttrAccount as String: key,
+            kSecReturnData as String: kCFBooleanTrue as Any,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+        
+        var dataTypeRef: AnyObject?
+        
+        SecItemCopyMatching(query as CFDictionary, &dataTypeRef)
+        guard let data = dataTypeRef as? Data else {
+            completion(.failure(.notFound))
+            return
+        }
+        let str = String(decoding: data, as: UTF8.self)
+        if str.isEmpty {
+            completion(.failure(.notFound))
+        } else {
+            completion(.success(str))
+        }
+    }
+    
+    func update(_ val: String, forKey key: String, completion: @escaping (UpdateResult) -> Void) {
+        let query: [String: AnyObject] = [
+            kSecAttrService as String: service as AnyObject,
+            kSecAttrAccount as String: key as AnyObject,
+            securityClass: kSecClassGenericPassword
+        ]
+        
+        let updateQuery: [String: AnyObject] = [
+            kSecValueData as String: val as AnyObject
+        ]
+        
+        let status = SecItemUpdate(query as CFDictionary, updateQuery as CFDictionary)
+        switch status {
+        case errSecSuccess:
+            completion(.success(()))
+        default:
+            completion(.failure(.failedToUpdate))
+        }
+    }
+    
+    func deleteValue(key: String, completion: @escaping (DeleteResult) -> Void) {
+        let query: [String: Any] = [
+            serviceName: service,
+            kSecAttrAccount as String: key,
+            kSecClass as String: kSecClassGenericPassword
+        ]
+        
+        let status = SecItemDelete(query as CFDictionary)
+        
+        switch status {
+        case errSecSuccess:
+            completion(.success(()))
+        default:
+            completion(.failure(.failedToDelete))
+        }
+    }
+    
+    // MARK: - Private Constants
+    private let securityClass = kSecClass as String
+    private let attributeLabel = kSecAttrLabel as String
+    private let serviceName = kSecAttrService as String
+    private let shouldReturnData = kSecReturnData as String
+    private let matchLimit = kSecMatchLimit as String
+    private let matchOnlyOne = kSecMatchLimitOne as String
+    private let data = kSecValueData as String
+}
+
+extension String: LocalizedError {}
